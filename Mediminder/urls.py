823df8c3a4556902d08323bb515371ccffdfbb35
mddrill <mddrill@matthews-mbp.attlocal.net>
2017-07-07 09:16:22 -0400

    first commit

---
+++ Mediminder/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^admin/', admin.site.urls),
+]
